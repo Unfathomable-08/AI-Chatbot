9452b8f090d0806c762fc00fc32ade40c5cb1004
Muhammad <muhammad124711@gmail.com>
2025-04-24 13:00:26 +0500

    model prepared

--- data_handler.py
+++ data_handler.py
@@ -1,10 +1,6 @@
+# Load and process dataset
 from data_load import dataset
-from data_proceeding import dialog_separator
-from data_proceeding import tokenization
-import numpy as np
-from nltk.tokenize import word_tokenize
-
-# Tokenize already existing dataset
+from data_proceeding import dialog_separator, tokenization, split_vector_sparse
 train = dataset["train"]
 test = dataset["test"]
 validation = dataset["validation"]
@@ -14,37 +10,16 @@ test_pairs = dialog_separator(test)
 validation_pairs = dialog_separator(validation)
 
 tokens = tokenization(train_pairs)
-
-# Assign a unique index to each token
 word2idx = {word: idx for idx, word in enumerate(tokens)}
 
-# Bag of Words Encoding Function
-def bow_vector(sentence):
-    vector = np.zeros(len(word2idx), dtype=np.float32)
-
-    for word in word_tokenize(sentence.lower()): #tokenize the sentence provided
-        if word in word2idx: #ignore words that are not in tokens
-            vector[word2idx[word]] += 1.0  
-
-    return vector
-
-# X, y spliting
-def split_vector(data_pairs):
-    X = [bow_vector(dialog) for dialog, act in data_pairs]
-    y = [act for dialog, act in data_pairs]
-
-    return X, y
-
-# Train Test Val Spliting
 def train_test_split(data):
     if data == "train":
-        X, y = split_vector(train_pairs) 
+        X, y = split_vector_sparse(train_pairs, word2idx)
     elif data == "test":
-        X, y = split_vector(test_pairs)  
+        X, y = split_vector_sparse(test_pairs, word2idx)
     elif data == "val":
-        X, y = split_vector(validation_pairs)  
-
+        X, y = split_vector_sparse(validation_pairs, word2idx)
     return X, y
 
 X, y = train_test_split("train")
-print(X[:6])
+print(X[:6])  # Sparse matrix slice
--- data_proceeding.py
+++ data_proceeding.py
@@ -3,43 +3,51 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 import string
+from scipy.sparse import csr_matrix
 
 nltk.download("punkt")
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('averaged_perceptron_tagger_eng')
 
-# Define unwanted Part Of Speech (POS) tags
 unwanted_tags = ['PRP', 'PRP$', 'DT', 'IN', 'CC', 'TO', 'MD', 'RP']
 
-# Separating into dialog and next act
 def dialog_separator(dataset):
     pairs = []
     for data in dataset:
         dialogs = data["dialog"]
-        acts = data["act"] 
+        acts = data["act"]
         for i in range(len(dialogs) - 1):
-            pairs.append((dialogs[i], acts[i + 1])) # Current dialog and next act
-    
+            pairs.append((dialogs[i], acts[i + 1]))
     return pairs
 
-# Tokenizing the dialogs
 def tokenization(data):
-    tokenized_words_puncts = [word_tokenize(dialog.lower()) for dialog, act in data]
+    tokens = set()
+    for dialog, _ in data:
+        words = [w for w in word_tokenize(dialog.lower()) if w not in string.punctuation]
+        pos_tags = pos_tag(words)
+        tokens.update(word for word, tag in pos_tags if tag not in unwanted_tags)
+    return list(tokens)
 
-    # Remove punctuation
-    tokenized_words = [[word for word in tokens if word not in string.punctuation] for tokens in tokenized_words_puncts]
+def bow_vector_sparse(sentence, word2idx):
+    indices = []
+    values = []
+    for word in word_tokenize(sentence.lower()):
+        if word in word2idx:
+            indices.append(word2idx[word])
+            values.append(1.0)
+    return indices, values
 
-    # Flatten the list
-    concatenated_tokens = [word for sublist in tokenized_words for word in sublist]
-
-    # POS tagging
-    pos_tags = pos_tag(concatenated_tokens)
-
-    # Filter tokens based on unwanted POS tags
-    filtered_tokens = [word for word, tag in pos_tags if tag not in unwanted_tags]
-
-    # Remove duplicates
-    tokens = list(set(filtered_tokens))
-
-    return tokens
+def split_vector_sparse(data_pairs, word2idx):
+    data = []
+    row_indices = []
+    col_indices = []
+    y = []
+    for i, (dialog, act) in enumerate(data_pairs):
+        indices, values = bow_vector_sparse(dialog, word2idx)
+        data.extend(values)
+        row_indices.extend([i] * len(values))
+        col_indices.extend(indices)
+        y.append(act)
+    X = csr_matrix((data, (row_indices, col_indices)), shape=(len(data_pairs), len(word2idx)))
+    return X, y
