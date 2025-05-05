2ba7b65dcbc997a90ca09e4c4ae3e447449b6aa1
Muhammad <muhammad124711@gmail.com>
2025-05-05 20:24:35 +0500

    added topic classification

--- topic_classifier.py
+++ topic_classifier.py
@@ -15,7 +15,7 @@ y = np.array(y).ravel()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-model = MultinomialNB(alpha=0.01)  # Updated to best alpha
+model = MultinomialNB(alpha=0.01, fit_prior=False)  # Updated to best alpha
 model.fit(X_train, y_train)
 
 # Evaluate
