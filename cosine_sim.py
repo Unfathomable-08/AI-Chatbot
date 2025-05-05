1b7b2e7805497bf1be194dc50e202b8ed6b7ab41
Muhammad <muhammad124711@gmail.com>
2025-05-05 19:30:23 +0500

    added topic classification

--- cosine_sim.py
+++ cosine_sim.py
@@ -3,16 +3,24 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from data_proceeding import dialog_separator, tokenization, split_vector_sparse, bow_vector_sparse
 from data_handler import xy_split, word2idx, all_pairs, topic2idx
-# from topic_classifier import predict_topic
+from topic_classifier import predict_topic
 
 def compute_cosine_similarity(input_sentence):
     predicted_topic = predict_topic(input_sentence)
 
+    # Convert input sentence to sparse vector
+    idx, val = bow_vector_sparse(input_sentence, word2idx)
+    input_vec = csr_matrix((val, ([0] * len(idx), idx)), shape=(1, len(word2idx)))
+
     # Prepare dataset matrix
     X, _ = xy_split()
 
+    # Make array of true false and filter
+    mask = y == topic2idx[predicted_topic]
+    X_filtered = X[mask]
+
     # Compute cosine similarities
-    similarities = cosine_similarity(input_vec, X).flatten()
+    similarities = cosine_similarity(input_vec, X_filtered).flatten()
 
     # Get index of most similar sentence
     best_idx = similarities.argmax()
