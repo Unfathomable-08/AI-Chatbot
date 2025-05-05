5993d8c196ab5398dd9bda388bb496418726f855
Muhammad <muhammad124711@gmail.com>
2025-05-05 14:37:15 +0500

    added topic classification

--- cosine_sim.py
+++ cosine_sim.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from data_proceeding import dialog_separator, tokenization, split_vector_sparse, bow_vector_sparse
 from data_handler import xy_split, word2idx, all_pairs
+# from topic_classifier import predict_topic
 
 def compute_cosine_similarity(input_sentence):
     # Convert input sentence to sparse vector
