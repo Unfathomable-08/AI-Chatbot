6c11948c702a88307667cbe7e5297b813d26e0fa
Muhammad <muhammad124711@gmail.com>
2025-05-05 20:20:02 +0500

    added topic classification

--- cosine_sim.py
+++ cosine_sim.py
@@ -7,21 +7,26 @@ from topic_classifier import predict_topic
 
 def compute_cosine_similarity(input_sentence):
     predicted_topic = predict_topic(input_sentence)
+    print(predicted_topic)
 
     # Convert input sentence to sparse vector
     idx, val = bow_vector_sparse(input_sentence, word2idx)
     input_vec = csr_matrix((val, ([0] * len(idx), idx)), shape=(1, len(word2idx)))
 
-    # Prepare dataset matrix
-    X, _ = xy_split()
+    # Filter when topic matches
+    X_filtered = []
+    X_index = []
+    for i in range(X.shape[0]):  
+        if y[i] == 6:
+            X_filtered.append(X[i])
+            X_index.append(i)
 
-    # Make array of true false and filter
-    mask = y == topic2idx[predicted_topic]
-    X_filtered = X[mask]
+    X_filtered = vstack(X_filtered)
 
     # Compute cosine similarities
     similarities = cosine_similarity(input_vec, X_filtered).flatten()
 
     # Get index of most similar sentence
     best_idx = similarities.argmax()
-    return all_pairs[best_idx + 1][0]
+    
+    return all_pairs[X_index[best_idx + 1]][0]
